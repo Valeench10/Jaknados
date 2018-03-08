bd289d2bb7d1736b5c7168f14a289b15fa6cf3c3
Valeench10 <32395262+Valeench10@users.noreply.github.com>
2018-03-07 19:29:04 -0500

    Juego

---
+++ Assets/TD2D/Scripts/Gameplay/Towers/Actions/TowerActionBuild.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Build the tower.
+/// </summary>
+public class TowerActionBuild : TowerAction
+{
+    // Tower prefab for this icon
+    public GameObject towerPrefab;
+	// Icon for disabled state
+	public GameObject disabledIcon;
+
+    // Text field for tower price
+    private Text price;
+	// Level manger has a list with allowed tower upgrades for this level.
+	private LevelManager levelManager;
+
+    /// <summary>
+    /// Awake this instance.
+    /// </summary>
+    void Awake()
+    {
+        price = GetComponentInChildren<Text>();
+		levelManager = FindObjectOfType<LevelManager>();
+		Debug.Assert(price && towerPrefab && enabledIcon && disabledIcon, "Wrong initial parameters");
+        // Display tower price
+        price.text = towerPrefab.GetComponent<Price>().price.ToString();
+		if (levelManager.allowedTowers.Contains(towerPrefab) == true)
+		{
+			enabledIcon.SetActive(true);
+			disabledIcon.SetActive(false);
+		}
+		else
+		{
+			enabledIcon.SetActive(false);
+			disabledIcon.SetActive(true);
+		}
+    }
+
+	/// <summary>
+	/// Clicked this instance.
+	/// </summary>
+	protected override void Clicked()
+	{
+		// Build the tower
+		Tower tower = GetComponentInParent<Tower>();
+		if (tower != null)
+		{
+			tower.BuildTower(towerPrefab);
+		}
+	}
+}
---
+++ Assets/TD2D/Scripts/abrirmenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class abrirmenu : MonoBehaviour {
+
+    public GameObject UISettingsmenu;
+    public GameObject UIInventory;
+
+    public void abrirsettings()
+    {
+        UISettingsmenu.SetActive(true);
+    }
+
+    public void cerrarsettings()
+    {
+        UISettingsmenu.SetActive(false);
+    }
+
+    public void legendarylevel()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void regularlevel()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
